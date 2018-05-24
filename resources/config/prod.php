4906e5231b4f30218688399436ccd4ceecb88ba2
Yaroslav Hnaiuk <yaroslav.hnatyuk@gmail.com>
2018-05-24 03:25:39 +0300

    Updated dependencies

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -4,20 +4,12 @@ $app['api.version'] = "v1";
 $app['api.endpoint'] = "/api";
 
 /**
- * SQLite database file
- */
-$app['db.options'] = array(
-    'driver' => 'pdo_sqlite',
-    'path' => realpath(ROOT_PATH . '/app.db'),
-);
-
-/**
  * MySQL
  */
-//$app['db.options'] = array(
-//  "driver" => "pdo_mysql",
-//  "user" => "root",
-//  "password" => "root",
-//  "dbname" => "prod_db",
-//  "host" => "prod_host",
-//);
+$app['db.options'] = array(
+    "driver" => "pdo_mysql",
+    "user" => "root",
+    "password" => "root",
+    "dbname" => "prod_db",
+    "host" => "prod_host",
+);
