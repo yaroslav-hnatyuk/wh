4d6be09021b60e0a60d102e5690fb97d31308b94
Yaroslav Hnatiuk <yaroslav_hnatiuk@epam.com>
2018-08-21 03:31:31 +0300

    Debug export

--- src/App/Services/ExportService.php
+++ src/App/Services/ExportService.php
@@ -22,6 +22,10 @@ class ExportService extends BaseService
             );
         }
 
+        echo "<pre>";
+        print_r($gUsers);
+        die;
+
         $gGroups = array();
         foreach ($menu as $item) {
             $gGroups[$item['group_id']] = array(
